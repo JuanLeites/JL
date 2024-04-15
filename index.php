c288efb947074c468a10368e6a554146756325a6
JuanLeites <123juan123123852@gmail.com>
2024-04-15 11:31:23 -0300

    log in
    primer log in sin terminar

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesión</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+
+    <form method="GET" >
+        <div class="contenedor">
+            <input type="text" name="usuario" placeholder="usuario">
+            <input type="password" name="contraseña" placeholder="contraseña">
+            <input type="submit" value="Iniciar Sesión">
+        </div>
+</form>
+</body>
+</html>
