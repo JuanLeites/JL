1b91b2715e50746ab5bc590ba48c93b571d6976b
JuanLeites <123juan123123852@gmail.com>
2024-10-20 20:55:49 -0300

    apis solucionadas
    Solución a la hora de obtener datos de la api.
    Algunos cambios en el style.css también.

--- apis/apisorteos.php
+++ apis/apisorteos.php
@@ -3,9 +3,9 @@ include_once("../coneccionBD.php");
 include_once("../chequeodelogin.php");
 if (isset($_GET["filtro"]) && $_GET["filtro"] != "undefined") {
     $_GET["filtro"] = str_replace('"', '´', $_GET["filtro"]); // reemplazamos la comilla doble por una comilla simple para evitar errores
-    $sorteosconsulta = mysqli_query($basededatos, 'SELECT * FROM sorteo WHERE (ID_SORTEO LIKE "%' . $_GET["filtro"] . '%" or Premio LIKE "%' . $_GET["filtro"] . '%" or Cantidad LIKE "%' . $_GET["filtro"] . '%" or Fecha_Realización LIKE "%' . $_GET["filtro"] . '%" ) and ACTIVO=TRUE  ORDER BY Fecha_realización');
+    $sorteosconsulta = mysqli_query($basededatos, 'SELECT * FROM sorteo WHERE (ID_SORTEO LIKE "%' . $_GET["filtro"] . '%" or Premio LIKE "%' . $_GET["filtro"] . '%" or Cantidad LIKE "%' . $_GET["filtro"] . '%" or Fecha_Realización LIKE "%' . $_GET["filtro"] . '%" ) and ACTIVO=TRUE  ORDER BY Premio');
 }else{
-    $sorteosconsulta = mysqli_query($basededatos,'SELECT * FROM sorteo WHERE ACTIVO = TRUE ORDER BY Fecha_realización'); 
+    $sorteosconsulta = mysqli_query($basededatos,'SELECT * FROM sorteo WHERE ACTIVO = TRUE ORDER BY Premio'); 
 }
     $sorteo=array();
     foreach($sorteosconsulta as $cadasorteo){
